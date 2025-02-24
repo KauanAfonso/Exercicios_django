55862e77b2c7632a8495cc0750db674b1cb43ccb
kauan <kauanafon3@gmail.com>
2025-02-24 09:53:30 -0300

    Criando tarefa

--- to_do_list/project/app/urls.py
+++ to_do_list/project/app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import mostrar_tarefa
+
+from .views import *
 
 urlpatterns = [
     path("", mostrar_tarefa, name="mostrar_tarefa"),
+    path("criar", criar_tarefa, name="criar_tarefa")
 ]
--- to_do_list/project/app/views.py
+++ to_do_list/project/app/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import *
 
 # Create your views here.
 def mostrar_tarefa(request):
     tarefas = Tarefa.objects.all()
     return render(request, "index.html" , {'tarefas':tarefas})
+
+def criar_tarefa(request):
+    if request.method == "POST":
+        descricao = request.POST.get('descricao')
+        status = request.POST.get('status')
+
+        Tarefa.objects.create(descricao=descricao , status=status)
+
+        return redirect("mostrar_tarefa")
+    
+    return render(request, "criar_tarefa.html")
