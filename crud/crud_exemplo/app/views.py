9d7387252d1333122af70d29d7ee10c189b003b7
kauanAfonso <kauanafon3@gmail.com>
2025-02-25 08:47:41 -0300

    Israel pediu os arquivos

--- crud/crud_exemplo/app/views.py
+++ crud/crud_exemplo/app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Item
+from .forms import ItemForm
+
+
+def item_read(request):
+    itens = Item.objects.all()
+    return render(request, "item_read.html", {'itens': itens})
 
 # Create your views here.
